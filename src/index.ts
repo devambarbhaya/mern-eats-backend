fae4e3a43b613d4d8253cf9835b31502fd7d0bb7
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-06 19:04:56 +0530

    database setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database"));
 
 const app = express();
-app.use(express.json())
-app.use(cors()); 
+app.use(express.json());
+app.use(cors());
 
 app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello!"})
-})
+  res.json({ message: "Hello!" });
+});
 
 app.listen(7000, () => {
-    console.log("Server started on localhost:7000")
-})
+  console.log("Server started on localhost:7000");
+});
