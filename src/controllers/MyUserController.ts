76740b5e5440c4378ea3063cc60d2965db08a81b
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-11 00:54:04 +0530

    get user API

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -43,7 +43,23 @@ const updateCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+
 export default {
   createCurrentUser,
   updateCurrentUser,
+  getCurrentUser,
 };
