44fb2b9d84121862b3d14afb04f15d31490588a6
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-10 11:10:21 +0530

    update user profile api

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -20,6 +20,30 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
