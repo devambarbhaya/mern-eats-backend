4ebbe5c444f0ad2f4fd60908099c3c34cd203ed0
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-11 11:25:46 +0530

    first deployment overview

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health OK!" });
+});
+
 app.use("/api/my/user", myUserRoutes);
 
 app.listen(7000, () => {
