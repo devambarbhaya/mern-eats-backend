9faea996e14337e740bb29039ddd09074ed17f9e
Devam Barbahya <barbhaya.devam@gmail.com>
2024-03-18 11:18:33 +0530

    create stripe session api

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import myUserRoutes from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import RestaurantRoute from "./routes/RestaurantRoute";
+import OrderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -29,6 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoutes);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", RestaurantRoute);
+app.use("/api/order", OrderRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
